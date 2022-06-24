f39f785510371137c5453b74bab4c5a69d540886
Ocean <s@ocean.lol>
2022-06-24 16:29:38 +0100

    lorra stuff 2

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,2 +1,22 @@
+import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+function Theme(props) {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    type: /*prefersDarkMode*/true ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+
+    return <ThemeProvider theme={theme}><CssBaseline />{props.children}</ThemeProvider>;
+}
 
 export default Theme;
